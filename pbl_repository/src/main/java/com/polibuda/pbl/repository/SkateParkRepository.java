e94a40a98a12837c0829203c81015348515cc940
Tomasz Macudzinski <tomaszmacudzinski@gmail.com>
2017-07-09 19:24:55 +0200

    mysql config file

--- pbl_repository/src/main/java/com/polibuda/pbl/repository/SkateParkRepository.java
+++ pbl_repository/src/main/java/com/polibuda/pbl/repository/SkateParkRepository.java
@@ -1,9 +1,12 @@
 package com.polibuda.pbl.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.polibuda.pbl.model.SkatePark;
 
+@Transactional
 public interface SkateParkRepository extends CrudRepository<SkatePark, Long> {
 
 }
--- pbl_repository/src/main/java/com/polibuda/pbl/repository/SkateSpotRepository.java
+++ pbl_repository/src/main/java/com/polibuda/pbl/repository/SkateSpotRepository.java
@@ -1,9 +1,12 @@
 package com.polibuda.pbl.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.polibuda.pbl.model.SkateSpot;
 
+@Transactional
 public interface SkateSpotRepository extends CrudRepository<SkateSpot, Long> {
 
 }
