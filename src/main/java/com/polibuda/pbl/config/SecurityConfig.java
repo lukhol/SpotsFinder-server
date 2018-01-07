5569d58bf4a9ab19f785779f30d5d0039c291b10
lukhol <lukasz.holdrowicz@gmail.com>
2018-01-07 13:26:33 +0100

    First approach to basic security with spring security.

---
+++ src/main/java/com/polibuda/pbl/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.polibuda.pbl.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("spot").password("finder").roles("ADMIN");
+	}
+
+	// Authorization : Role -> Access
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic().and().authorizeRequests().antMatchers("/**").hasRole("ADMIN").and()
+				.csrf().disable().headers().frameOptions().disable();
+	}
+}
